fd334d95c64a5acd6e4bc855371d0de685c267ba
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-05-21 16:37:24 -0600

    Routing, some pages and configuring tailwind

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+export const Login = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Inicia Sesión y Administra tus{' '}
+          <span className="text-black">Pacientes</span>
+        </h1>
+      </div>
+      <div>2</div>
+    </>
+  );
+};
